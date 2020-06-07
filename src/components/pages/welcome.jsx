fe73d0d41e9aab25df8c3e57298fa2afae57a016
mayowa_ola <mayowaoladunjoye@gmail.com>
2020-06-07 23:40:36 +0100

    made welcome image responsive

--- src/components/pages/welcome.jsx
+++ src/components/pages/welcome.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Welcome = () => {
     return ( 
         <div className="text-center mt-4">
-            <img  height="300px" src={process.env.PUBLIC_URL + '/images/welcome.jpg'}alt="profily logo"/>
+            <img className="img-fluid" height="300px" src={process.env.PUBLIC_URL + '/images/welcome.jpg'}alt="profily logo"/>
             {/* <h1 className="text-info">WELCOME TO PROFILY</h1> */}
         </div>
      );
