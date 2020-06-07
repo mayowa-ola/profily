6beeeeb7a2b4f4ef5ecfcbdfe60b9f115383794a
mayowa_ola <mayowaoladunjoye@gmail.com>
2020-06-07 17:12:09 +0100

    http ui fix

--- src/services/httpsServices.jsx
+++ src/services/httpsServices.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import logger from './logService';
 
-// axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-axios.defaults.baseURL = 'http://localhost:3001/api';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+// axios.defaults.baseURL = 'http://localhost:3001/api';
  
 //to set the header for all request
 axios.interceptors.response.use(null, error =>{
